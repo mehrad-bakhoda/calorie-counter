2574dbe9722fc7e8ef32533d1165aa9048f09b76
BlueShdw <no2.nemesis@gmail.com>
2022-06-13 13:50:49 +0430

    added fontawesome

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Calorie Counter</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Calorie Counter" key="title" />
+      </Head>
       <div className={styles.card}>
         <input />
+        <FontAwesomeIcon
+          icon={faSearch}
+          style={{ fontSize: 30, color: "gray" }}
+        />
       </div>
     </div>
   );
