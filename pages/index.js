8b27bbbe5affa4b1633acefa13cfc467af449adb
BlueShdw <no2.nemesis@gmail.com>
2022-06-13 14:45:34 +0430

    functionality kinda completed not connected to the correct api tho

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,60 @@ import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (search) {
+      setLoading(true);
+      fetch(`https://api.github.com/search/repositories?q=${search}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setResults(data.items);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err);
+          setLoading(false);
+        });
+    }
+  }, [search]);
   return (
     <div className={styles.container}>
       <Head>
         <title>Calorie Counter</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Calorie Counter" key="title" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.card}>
-        <input />
-        <FontAwesomeIcon
-          icon={faSearch}
-          style={{ fontSize: 30, color: "gray" }}
-        />
-      </div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <FontAwesomeIcon icon={faSearch} /> Calorie Counter
+        </h1>
+        <div className={styles.search}>
+          <input
+            type="text"
+            placeholder="Search for a food"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : (
+          <ul className={styles.results}>
+            {results.map((result) => (
+              <li key={result.id}>
+                <a href={result.html_url}>{result.full_name}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </div>
   );
 }
